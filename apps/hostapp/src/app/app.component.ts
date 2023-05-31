45524f1c764ebab25ff474c46e34e247afa8f509
cyleecode <chunyitlee@gmail.com>
2023-05-31 16:46:43 +0800

    Complete: Update main component
    Main component redirect to route /login (mfe)

--- apps/hostapp/src/app/app.component.ts
+++ apps/hostapp/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '@mobileapp/shared/data-access-user';
+import { distinctUntilChanged } from 'rxjs';
 
 @Component({
-  selector: 'mobileapp-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  selector: 'mobileapp-ng-root',
+  template: `
+    <div class="dashboard-nav">Admin Dashboard</div>
+    <div *ngIf="isLoggedIn$ | async; else signIn">
+      You are authenticated so you can see this content.
+    </div>
+    <ng-template #signIn><router-outlet></router-outlet></ng-template>
+  `
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'hostapp';
+  isLoggedIn$ = this.userService.isUserLoggedIn$;
+  constructor(private userService: UserService, private router: Router) {}
+  ngOnInit() {
+    this.isLoggedIn$.pipe(distinctUntilChanged()).subscribe(async (loggedIn) => {
+      // Queue the navigation after initialNavigation blocking is completed
+      setTimeout(() => {
+        if (!loggedIn) {
+          this.router.navigateByUrl('login');
+        } else {
+          this.router.navigateByUrl('');
+        }
+      });
+    });
+  }
 }
